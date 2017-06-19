dabb479f4e40f28e80378c89fead158dce880143
Neioumi <neioumi@me.com>
2017-06-19 23:47:03 +0900

    Add score display

--- Assets/ScoreCounter.cs
+++ Assets/ScoreCounter.cs
@@ -4,19 +4,52 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreCounter : MonoBehaviour {
-	// スコア初期化
-	private int score = 0;
+
 	// スコアを表示するテキスト
 	private GameObject scoreText;
+	// スコア
+	private int score;
 
 	// Use this for initialization
 	void Start () {
 		// シーン中のオブジェクトを取得
-		this.scoreText = GameObject.Find("ScoreText");
+		scoreText = GameObject.Find("ScoreText");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.scoreText.GetComponent<Text>().text = score.ToString();
+		
+	}
+
+	void OnCollisionEnter(Collision other) {
+		// ボールが星・雲に当たった時の得点
+		switch(other.gameObject.tag) {
+		case "SmallStarTag":
+			score += 10;
+			Debug.Log("Collied to Small Star(+10pt). Score:" + score);
+			break;
+		case "LargeStarTag":
+			score += 30;
+			Debug.Log("Collied to Large Star(+30pt). Score:" + score);
+			break;
+		case "SmallCloudTag":
+			score += 20;
+			Debug.Log("Collied to Small Cloud(+20pt). Score:" + score);
+			break;
+		case "LargeCloudTag":
+			score += 50;
+			Debug.Log("Collied to Large Cloud(+50pt). Score:" + score);
+			break;
+		default:
+			Debug.Log("Collied to something.");
+			break;
+		}
+
+		// int型をString変換
+		string scoreString = score.ToString();
+		// テキストを表示
+		scoreText.GetComponent<Text>().text = scoreString;
+		// Debug.Log(scoreString);
+		// Debug.Log(gameObject);
 	}
 }
